c41d104a1d53b93af950fa1fd8d57d1d9eb3265b
dcslagel <dcs@mailworks.org>
2020-11-10 02:04:58 -0700

    Add importlib_resources to requirements
    This change adds importlib_resources to requirements.txt for the purpose
    of working with python3 versions before 3.7.  Once 3.6 is nolonger
    supported this change can be removed.
    https://importlib-resources.readthedocs.io/en/latest/

--- alaska/get_data_path.py
+++ alaska/get_data_path.py
@@ -2,6 +2,7 @@
 Utility function to reliably get AlasKA data files from the package data
 location whether it is a regular package install (pip install alaska) or a
 developer/editable install from the alaska repo (pip install -e .)
+also see: https://importlib-resources.readthedocs.io/en/latest/
 """
 try:
     import importlib.resources as pkg_resources
