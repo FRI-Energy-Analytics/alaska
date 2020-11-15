040471ab0ba74e3f45c79e42445bb0090dd2bc1e
dcslagel <dcs@mailworks.org>
2020-11-14 20:54:03 -0700

    Fix KeyError in example.py
    - Add looping through values list
    - Add looping through las.curves list
    - Compare curve.mnemonic to value and if they match add curve.descr to l2
    - Add report headers to clarify output results

--- tutorials/example.py
+++ tutorials/example.py
@@ -15,16 +15,31 @@ a = Alias()
 parsed, not_found = a.parse(path)
 las = lasio.read(path)
 l, l2, l3 = [], [], []
-for key, value in parsed.items():
+
+for key, values in parsed.items():
     l.append(key)
-    l2.append(str(las.curves[key].descr))
-    l3.append(value)
+    for value in values:
+        for curve in las.curves:
+            if curve.mnemonic.lower() == value.lower():
+                l2.append(str(curve.descr))
+        l3.append(value)
+
 output_df = pd.DataFrame({"mnemonic": l, "description": l2, "parsed_mnemonic": l3})
+
+
 l, l2, l3, lst = [], [], [], []
 for i in not_found:
     l.append(i)
     l2.append(str(las.curves[i].descr))
     l3.append(str(las.curves[i].unit).lower())
 output_df2 = pd.DataFrame({"mnemonics": l, "description": l2, "units": l3})
+
+
+print("\n-----------------------------------------------------------")
+print("Found")
+print("-----------------------------------------------------------")
 print(output_df)
+print("\n-----------------------------------------------------------")
+print("Not-Found")
+print("-----------------------------------------------------------")
 print(output_df.loc[output_df["mnemonic"].isin(output_df2.mnemonics)])
