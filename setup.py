e0a5c345f1cd00479406c5b4a5ca21dd58a02a77
dingdonged <42194656+dingdonged@users.noreply.github.com>
2020-11-20 14:58:12 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ CLASSIFIERS = [
     "Topic :: Scientific/Engineering",
     "Topic :: Software Development :: Libraries",
     "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "License :: OSI Approved :: {}".format(LICENSE),
